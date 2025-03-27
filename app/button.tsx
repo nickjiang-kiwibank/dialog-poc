e58c0f613d46c1211f23ad428f00f753e7a8f2fd
Nick Jiang <Nick.Jiang@kiwibank.co.nz>
2025-03-27 13:21:50 +1300

    feat: dialog

---
+++ app/button.tsx
@@ -0,0 +1,49 @@
+import { Copy } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { toast } from "sonner";
+
+export function DialogCloseButton() {
+    const onClick = () => {
+        toast("Event has been created.")
+    }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Delete</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Delete</DialogTitle>
+          <DialogDescription>Delete txns</DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2"></div>
+          <Button  onClick={onClick}>Toast</Button>
+
+        </div>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+          <DialogClose asChild>
+          <Button type="submit" onClick={onClick}>Save changes</Button>
+          </DialogClose>
+
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
